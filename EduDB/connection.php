74ac2f66dfc595737b3355061412d8db3423b71a
Matthew Ary <matthewj.c.ary@gmail.com>
2016-02-15 12:39:28 -0700

    Again. Ditto.

--- EduDB/connection.php
+++ EduDB/connection.php
@@ -11,9 +11,11 @@ class Db {
          $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
          if (getenv('OPENSHIFT_MYSQL_DB_HOST')) {
             define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-            define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-            self::$instance = new PDO('mysql:host=localhost;port='.DB_PORT.'dbname=schoolapp',
-                                   'schoolapp', 'forest', $pdo_options);
+            define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
+            define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+            define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+            self::$instance = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname=schoolapp',
+                                   DB_USER, DB_PASS, $pdo_options);
          } else {
             self::$instance = new PDO('mysql:host=localhost;dbname=schoolapp',
                                    'schoolapp', 'forest', $pdo_options);
