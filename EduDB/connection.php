b0eb8bb193ffa62644bd147a5fdeb324718d6ecb
Matthew Ary <matthewj.c.ary@gmail.com>
2016-02-15 12:35:05 -0700

    OpenShift DB Connection

--- EduDB/connection.php
+++ EduDB/connection.php
@@ -12,7 +12,7 @@ class Db {
          if (getenv('OPENSHIFT_MYSQL_DB_HOST')) {
             define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
             define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-            self::$instance = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.'dbname=schoolapp',
+            self::$instance = new PDO('mysql:host=localhost;port='.DB_PORT.'dbname=schoolapp',
                                    'schoolapp', 'forest', $pdo_options);
          } else {
             self::$instance = new PDO('mysql:host=localhost;dbname=schoolapp',
